f50f859dc92a5aa8756e52121cb395b45a91d35b
muhammaddariazzidane <dariazzidane@gmail.com>
2024-05-31 20:21:23 +0700

    feat(post): update route and add middleware verify token

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import express from 'express'
 import 'dotenv/config.js'
 import './utils/connection.js'
 import { AuthRouter } from './routes/auth.route.js'
-import { ArticleRouter } from './routes/article.route.js'
+import { PostRouter } from './routes/post.route.js'
 import { CommentRouter } from './routes/comment.route.js'
 import { JournalRouter } from './routes/journal.route.js'
+import { verifyToken } from './middleware/verifyToken.middleware.js'
 
 const port = process.env.PORT
 const app = express()
@@ -20,9 +21,9 @@ app.use(
 app.get('/', (_, res) => res.json({ message: 'Hello world' }))
 
 app.use('/auth', AuthRouter)
-app.use('/articles', ArticleRouter)
-app.use('/comments', CommentRouter)
-app.use('/journals', JournalRouter)
+app.use('/posts', verifyToken, PostRouter)
+app.use('/comments', verifyToken, CommentRouter)
+app.use('/journals', verifyToken, JournalRouter)
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
